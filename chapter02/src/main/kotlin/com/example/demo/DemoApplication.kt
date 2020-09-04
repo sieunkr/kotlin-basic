a52cc55ac2f5b969fbf79c8be43008d4de843afa
sieunkim <sieunkr@gmail.com>
2020-09-04 11:03:02 +0900

    코틀린 기초

---
+++ chapter02/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,25 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    println(max(1, 2))
+    runApplication<DemoApplication>(*args)
+}
+
+/* 블록이 본문인 함수, 반드시 반환 타입을 지정하고 return 문을 사용해 반환 값을 명시해야 한다.
+fun max(a: Int, b: Int): Int {
+    return if (a > b) a else b
+}
+*/
+
+//식이 본문인 함수
+//fun max(a: Int, b: Int): Int = if (a > b) a else b
+
+//반환 타입을 생략할 수도 있음, 타입 추론(type inference), 식이 본문인 함수의 반환 타입만 생략 가능
+fun max(a: Int, b: Int) = if (a > b) a else b
+
