d134b2f8a06d6958e638f221baa12a31be7dfaed
sieunkim <sieunkr@gmail.com>
2020-09-06 23:14:50 +0900

    Enum.

--- chapter02/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ chapter02/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -18,6 +18,10 @@ fun main(args: Array<String>) {
 
     val rectangle = Rectangle(50, 50)
     println(rectangle.isSquare)
+
+    println(Color.RED.rgb())
+
+    println(mix(Color.RED, Color.YELLOW))
 }
 
 /* 블록이 본문인 함수, 반드시 반환 타입을 지정하고 return 문을 사용해 반환 값을 명시해야 한다.
@@ -32,3 +36,15 @@ fun max(a: Int, b: Int): Int {
 //반환 타입을 생략할 수도 있음, 타입 추론(type inference), 식이 본문인 함수의 반환 타입만 생략 가능
 fun max(a: Int, b: Int) = if (a > b) a else b
 
+fun getMnemonic(color: Color) =
+        when (color) {
+            Color.RED -> "Richard"
+            Color.YELLOW -> "Test"
+            Color.ORANGE -> "Of"
+        }
+
+fun mix(c1: Color, c2: Color) =
+        when (setOf(c1, c2)) {
+            setOf(Color.RED, Color.YELLOW) -> Color.ORANGE
+            else -> throw Exception("Dirty color")
+        }
